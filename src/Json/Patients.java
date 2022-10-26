55f4cf6ce7145dfb36713e8a70e6d001a1abfe10
moxAnustasiya <nassstasiya@gmail.com>
2022-10-26 16:38:27 +0300

    Создание класса для хранения пациентов в Json

---
+++ src/Json/Patients.java
@@ -0,0 +1,13 @@
+package Json;
+
+import java.util.List;
+
+public class Patients
+{
+    protected List<Patient> patients;
+
+    public Patients(List<Patient> patients)
+    {
+        this.patients = patients;
+    }
+}
