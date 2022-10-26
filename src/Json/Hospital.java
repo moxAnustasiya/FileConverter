07bd93b0398c9683d75f9e0294f06e6b80a83612
moxAnustasiya <nassstasiya@gmail.com>
2022-10-26 16:39:32 +0300

    Создание класса для хранения больницы в Json

---
+++ src/Json/Hospital.java
@@ -0,0 +1,21 @@
+package Json;
+
+import java.util.List;
+
+public class Hospital
+{
+    static Patients patients;
+
+    public Hospital(Patients patients)
+    {
+        this.patients = patients;
+    }
+    public static List<Patient> getPatients()
+    {
+        return patients.patients;
+    }
+    public static Patient get (int i)
+    {
+        return patients.patients.get(i);
+    }
+}
